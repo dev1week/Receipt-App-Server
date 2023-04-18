2f08ea6f2e3422ae6c8d46c0de3d80ece2e9e927
dev1week <rondo2860@gmail.com>
2023-04-18 15:12:30 +0900

    #1 -8 Reciept 도메인 관련 라우팅 처리

---
+++ mainApp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import ReceiptList, ReceiptsDetail
+
+urlpatterns = [
+    path('receipts/', ReceiptList.as_view()),
+    path('receipt/<int:pk>',  ReceiptsDetail.as_view())
+    
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
