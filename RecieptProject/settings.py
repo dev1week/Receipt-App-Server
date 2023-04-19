cfe1cdf844864bb631c6e687295d91b55f2a1f46
dev1week <rondo2860@gmail.com>
2023-04-19 21:04:05 +0900

    #3 -1 db 환경 변경

--- RecieptProject/settings.py
+++ RecieptProject/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'RecieptProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = my_settings.PROD_TEST_DATABASES
+DATABASES = my_settings.DEV_DATABASES
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
